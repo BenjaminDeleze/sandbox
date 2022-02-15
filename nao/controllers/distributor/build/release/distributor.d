440c7b9fd9f5e6709d1585e275caacbf26a19bc2
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-02-15 08:37:05 +0100

    remove nao

--- nao/controllers/distributor/build/release/distributor.d
+++
@@ -1,6 +0,0 @@
-build/release/distributor.o: distributor.c \
- /home/benjamin/webots2/include/controller/c/webots/led.h \
- /home/benjamin/webots2/include/controller/c/webots/types.h \
- /home/benjamin/webots2/include/controller/c/webots/robot.h \
- /home/benjamin/webots2/include/controller/c/webots/nodes.h \
- /home/benjamin/webots2/include/controller/c/webots/touch_sensor.h
