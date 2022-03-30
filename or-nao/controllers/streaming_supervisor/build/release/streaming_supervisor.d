0b19973b67ff1538d8977b02ffac1b9e7d272dca
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-03-30 08:51:58 +0200

    or-nao example

---
+++ or-nao/controllers/streaming_supervisor/build/release/streaming_supervisor.d
@@ -0,0 +1,7 @@
+build/release/streaming_supervisor.o: streaming_supervisor.c \
+ /home/cyberbotics/webots/include/controller/c/webots/robot.h \
+ /home/cyberbotics/webots/include/controller/c/webots/types.h \
+ /home/cyberbotics/webots/include/controller/c/webots/nodes.h \
+ /home/cyberbotics/webots/include/controller/c/webots/supervisor.h \
+ /home/cyberbotics/webots/include/controller/c/webots/plugins/robot_window/default.h \
+ /home/cyberbotics/webots/include/controller/c/webots/plugins/robot_window/robot_wwi.h
