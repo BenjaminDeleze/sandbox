2fbba6a5951481c323a9782c8671f96ec740f6a2
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-04-27 08:29:42 +0200

    alert

--- SecurityRW/plugins/robot_windows/interact_with_environment/interact_with_environment.js
+++ SecurityRW/plugins/robot_windows/interact_with_environment/interact_with_environment.js
@@ -2,5 +2,5 @@ import RobotWindow from 'https://cyberbotics.com/wwi/R2022b/RobotWindow.js';
 
 // Initialize the RobotWindow class in order to communicate with the robot.
 window.onload = function() {
-  console.log(localStorage)
+  alert("This is a test alert")
 };
