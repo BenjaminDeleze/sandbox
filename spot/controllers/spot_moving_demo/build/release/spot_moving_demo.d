83dbd039190fa3681e9807b9dc0f9704de53f411
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2022-11-24 12:28:46 +0100

    test

---
+++ spot/controllers/spot_moving_demo/build/release/spot_moving_demo.d
@@ -0,0 +1,9 @@
+build/release/spot_moving_demo.o: spot_moving_demo.c \
+ /home/benjamin/webots/include/controller/c/webots/camera.h \
+ /home/benjamin/webots/include/controller/c/webots/camera_recognition_object.h \
+ /home/benjamin/webots/include/controller/c/webots/types.h \
+ /home/benjamin/webots/include/controller/c/webots/device.h \
+ /home/benjamin/webots/include/controller/c/webots/nodes.h \
+ /home/benjamin/webots/include/controller/c/webots/led.h \
+ /home/benjamin/webots/include/controller/c/webots/motor.h \
+ /home/benjamin/webots/include/controller/c/webots/robot.h
